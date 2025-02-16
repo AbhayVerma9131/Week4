8549b5bb9488911968d0c49bc728748a5538116e
AbhayVerma9131 <abhay45ver@gmail.com>
2025-02-17 00:07:28 +0530

    [Abhay] Add. JUnit test for DependencyInjection class

---
+++ annotationreflection/src/test/java/abhay/reflection/advanced/DependencyInjectionTest.java
@@ -0,0 +1,13 @@
+package abhay.reflection.advanced;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DependencyInjectionTest {
+    @Test
+    void testDependencyInjection() throws Exception {
+        Application app = new Application();
+        DependencyInjection.injectDependencies(app);
+        assertDoesNotThrow(app::run);
+    }
+}
